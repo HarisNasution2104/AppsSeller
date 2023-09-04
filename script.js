a6d2f2660f7f7b1f4d1d76b6a5cfee071f70f9de
Nasution <120891496+HarisNasution2104@users.noreply.github.com>
2023-09-04 19:37:41 +0700

    main

---
+++ script.js
@@ -0,0 +1,9 @@
+// Fungsi untuk membuka popup login
+function openLoginPopup() {
+    document.getElementById("loginOverlay").style.display = "flex";
+}
+
+// Fungsi untuk menutup popup login
+function closeLoginPopup() {
+    document.getElementById("loginOverlay").style.display = "none";
+}
