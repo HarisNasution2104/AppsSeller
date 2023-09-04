bf1d8065b30d3800de57be3b47d9267d902bbfbb
Nasution <120891496+HarisNasution2104@users.noreply.github.com>
2023-09-04 19:43:54 +0700

    main

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function openLoginPopup() {
 function closeLoginPopup() {
     document.getElementById("loginOverlay").style.display = "none";
 }
+
+// Fungsi untuk menampilkan popup login secara otomatis
+function showAutoLoginPopup() {
+    // Di sini Anda dapat menentukan kondisi kapan popup harus muncul secara otomatis
+    var shouldShowPopup = true;
+
+    if (shouldShowPopup) {
+        openLoginPopup();
+    }
+}
+
+// Panggil fungsi untuk menampilkan popup login secara otomatis
+showAutoLoginPopup();
